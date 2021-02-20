ace1837781aa899da3f86657ab64ea129d0730f1
wilbdhm <mi.bar.2001@gmail.com>
2021-02-21 00:06:04 +0100

    Increased buffer size. Minor optimisations. Formatted code.

--- src/main.c
+++ src/main.c
@@ -1,23 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(int argc, char * argv[])
-{
-    char* buffer = NULL;
-    buffer = (char*)malloc(100 * sizeof(char));
+int main(int argc, char * argv[]) {
+    char * buffer = NULL;
+    buffer = (char *)malloc(8192 * sizeof(char));
 
-    if(fgets(buffer, 100, stdin) == NULL)
+    if (!buffer) {
+        fputs("Failed to allocate memory.\n", stderr);
+        return -1;
+    }
+
+    if (fgets(buffer, 8192, stdin) == NULL)
         return 1;
 
-    for(int i = 0, j = 0; i <= strlen(buffer); i++)
-    {
-        if (!isspace(buffer[i]))
-        {
+    for (int i = 0, j = 0, l = strlen(buffer); i <= l; i++) {
+        if (!isspace(buffer[i])) {
             buffer[j] = buffer[i];
             j++;
         }
     }
-    buffer = (char*)realloc(buffer, strlen(buffer) * sizeof(char));
-    
-    if (buffer != NULL)
-        free(buffer);
+
+    size_t len = strlen(buffer); // might be useful later on
+    buffer = (char *)realloc(buffer, len * sizeof(char));
+    free(buffer);
     return 0;
 }
