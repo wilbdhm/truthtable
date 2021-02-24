f0043f654817a7c2b5997ad013bc4a476555b1ca
wilbdhm <mi.bar.2001@gmail.com>
2021-02-24 21:33:25 +0100

    Work on lexer started. Everything important is in main() for now.

--- src/main.c
+++ src/main.c
@@ -2,13 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
+#include "token.h"
+
+const char * ALLOC_FAIL_MSG = "Failed to allocate memory.\n";
 
 int main(int argc, char * argv[]) {
     char * buffer = NULL;
     buffer = (char *)malloc(8192 * sizeof(char));
 
     if (!buffer) {
-        fputs("Failed to allocate memory.\n", stderr);
+        fputs(ALLOC_FAIL_MSG, stderr);
         return -1;
     }
 
@@ -24,6 +28,63 @@ int main(int argc, char * argv[]) {
 
     size_t len = strlen(buffer); // might be useful later on
     buffer = (char *)realloc(buffer, len * sizeof(char));
+    struct Token * lex_out = (struct Token *)malloc(len * sizeof(struct Token));
+
+    if (!lex_out) {
+        fputs(ALLOC_FAIL_MSG, stderr);
+        return -1;
+    }
+
+    // lexing
+    {
+        size_t lex_i = 0;
+        size_t tok_i = 0;
+
+        for (; lex_i < len; ++lex_i) {
+            switch (buffer[lex_i]) {
+                case '(':
+                    lex_out[tok_i] = TOK_LB;
+                    break;
+
+                case ')':
+                    lex_out[tok_i] = TOK_RB;
+                    break;
+
+                case '~':
+                    lex_out[tok_i] = TOK_NOT;
+                    break;
+
+                case '&':
+                    lex_out[tok_i] = TOK_AND;
+                    break;
+
+                case '|':
+                    lex_out[tok_i] = TOK_OR;
+                    break;
+
+                case '=':
+                    if (lex_i >= len) {
+                        // big error
+                        // crash or something
+                        // goto for now
+                        fprintf(stderr, "OVERFLOW AAAAAAA\n");
+                        goto panic;
+                    }
+
+                    break;
+
+                case '<':
+                    break;
+
+                default:
+                    break;
+            }
+
+            ++tok_i;
+        }
+    }
+panic:
+    free(lex_out);
     free(buffer);
     return 0;
 }
